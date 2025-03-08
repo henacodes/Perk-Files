3223d1285534af9622f8e40c05c2791afe27c3e3
kirakos <henacodes@gmail.com>
2025-03-08 19:38:44 +0300

    register page form done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 	import '../app.css';
+	import { Button } from 'bits-ui';
+	import { Label, Switch } from 'bits-ui';
+	import { Progress, useId } from 'bits-ui';
+	import { onMount } from 'svelte';
 	let { children } = $props();
+
+	let value = $state(13);
+	const labelId = useId();
+
+	onMount(() => {
+		const timer = setTimeout(() => (value = 66), 500);
+		return () => clearTimeout(timer);
+	});
 </script>
 
 <div class=" bg-secondary">
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,20 +1,54 @@
 <script lang="ts">
+	import { authClient } from '$lib/auth-client';
+
+	const formData = { email: '', password: '', firstName: '', lastName: '', image: null };
 </script>
 
-<div class=" flex justify-center p-6">
+<div class="  p-6">
+	<p class="text-2xl font-bold text-center">Sign-In</p>
 	<form action="" class="flex flex-col w-full">
 		<input
 			type="email"
 			class=" w-full border-b border-primary my-3 p-3 focus-visible:outline-none"
 			placeholder="Email"
+			bind:value={formData.email}
 		/>
 		<input
 			type="password"
 			class=" w-full border-b border-primary my-3 p-3 focus-visible:outline-none"
 			placeholder="password"
+			bind:value={formData.password}
 		/>
 
-		<button class=" border border-primary p-2 hover:shadow-[4px_4px_#5e503f] transition ease-in-out"
+		<div class="flex">
+			<input
+				type="text"
+				class=" w-full border-b mx-1 border-primary my-3 p-3 focus-visible:outline-none"
+				placeholder="First Name"
+				bind:value={formData.firstName}
+			/>
+			<input
+				type="text"
+				class=" w-full border-b mx-1 border-primary my-3 p-3 focus-visible:outline-none"
+				placeholder="Last Name ( optional )"
+				bind:value={formData.lastName}
+			/>
+		</div>
+		<div class="my-3">
+			<p class="my-1">Profile Picture</p>
+			<input
+				type="file"
+				name="profile"
+				placeholder="Profile Pic"
+				class="bg-primary/20 cursor-pointer rounded p-2 mb-6"
+				id=""
+				bind:files={formData.image}
+			/>
+		</div>
+
+		<button
+			onclick={() => console.log(formData)}
+			class=" border border-primary p-2 hover:shadow-[4px_4px_#5e503f] transition ease-in-out"
 			>Register
 		</button>
 	</form>
